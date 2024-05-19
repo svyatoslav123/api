b388a76edf375eb503ab25b2f32384267ae528de
svyatoslav123 <151392816+svyatoslav123@users.noreply.github.com>
2024-05-19 12:44:14 +0300

    DDDDDDDDDDDDDDDDDDDDDD

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
 import requests
+app = QApplication([])
+
+window = QWidget()
+window.resize(200, 100)
+
+otrym = QPushButton("Отримати результат")
+valute = QLineEdit("")
+data = QLineEdit("")
+rezult = QLineEdit("")
+
+main_line = QVBoxLayout()
+
+h1 = QHBoxLayout()
+h2 = QHBoxLayout()
+h3 = QHBoxLayout()
+h4 = QHBoxLayout()
+
+
+h1.addWidget(valute)
+h2.addWidget(data)
+h3.addWidget(rezult)
+h4.addWidget(otrym)
+
+
+main_line.addLayout(h1)
+main_line.addLayout(h2)
+main_line.addLayout(h3)
+main_line.addLayout(h4)
+
+def dd():
+    valcode = valute.text()
+    date = data.text()
+    url1 = f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={valcode}&date={date}&json"
+    r = requests.get(url1)
+    if r.status_code == 200:
+        res = r.json()
+        rezult.setText(str(res[0]["rate"]))
 
 
 
 
-valut = "USD"
-date = "20240126"
-url1 = f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={valut}&date={date}&json"
-r = requests.get(url1)
-if r.status_code == 200:
-    data = r.json()
-    print(data)
-else:
-    print("помилка")
+
+
+
+
+otrym.clicked.connect(dd)
+window.setLayout(main_line)
+window.show()
+app.exec()
